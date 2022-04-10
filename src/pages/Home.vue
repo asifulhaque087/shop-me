609e7cbd7ddca50da5306f95b75f3d6f71c2b3b6
asiful086 <asifulhaque086@gmail.com>
2022-04-10 13:33:58 +0600

    add pages directory

---
+++ src/pages/Home.vue
@@ -0,0 +1,83 @@
+<template>
+  <div class="flex flex-col md:flex-row px-[8%] gap-10 pt-10">
+    <!-- left -->
+    <div class="w-[300px] overflow-hidden shrink-0">
+      <!-- todays deal -->
+      <div class="pt-10">
+        <todays-deal />
+      </div>
+      <!-- Bestseller   Products -->
+      <div></div>
+    </div>
+    <!-- right -->
+    <div class="grow shrink overflow-hidden">
+      <div class="">
+        <!-- hero -->
+        <hero-slider />
+        <!-- banner -->
+        <div class="flex items-center gap-5 py-10">
+          <!-- left -->
+          <div>
+            <img
+              src="https://velikorodnov.com/wordpress/shopmewp/classic/wp-content/uploads/2015/04/banner_img_1.jpg"
+              alt=""
+            />
+          </div>
+          <!-- right -->
+
+          <div>
+            <img
+              src="https://velikorodnov.com/wordpress/shopmewp/classic/wp-content/uploads/2015/04/banner_img_2.jpg"
+              alt=""
+            />
+          </div>
+        </div>
+        <!-- Medicine & Health -->
+        <div>
+          <h1 class="text-2xl font-bold text-[#333] mb-3">Medicine & Health</h1>
+          <div class="flex gap-1">
+            <span
+              class="rounded-t p-2 bg-white text-[#333] border-l border-r border-t hover:bg-[#018bc8] hover:text-white"
+            >
+              Feminine Care
+            </span>
+            <span
+              class="rounded-t p-2 bg-white text-[#333] border-l border-r border-t"
+            >
+              Food Care
+            </span>
+
+            <span
+              class="rounded-t p-2 bg-white text-[#333] border-l border-r border-t"
+            >
+              Hair Care
+            </span>
+          </div>
+          <example-slider />
+          <div class="bg-white border-b border-l border-r p-5">
+            <button
+              type="button"
+              class="whitespace-nowrap text-[#4ac4fa] bg-[#f8f8f8] hover:bg-[#018bc8] hover:text-[#f8f8f8] focus:ring-4 focus:ring-blue-300 font-medium rounded text-sm px-5 py-2.5 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
+            >
+              View All Products
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import ExampleSlider from '../components/ExampleSlider.vue';
+import HeroSlider from '../components/HeroSlider.vue';
+import TodaysDeal from '../components/TodaysDeal.vue';
+
+export default {
+  components: {
+    ExampleSlider,
+    TodaysDeal,
+    HeroSlider,
+  },
+};
+</script>
