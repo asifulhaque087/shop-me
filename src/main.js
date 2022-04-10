7a3cddca257618759cd9ce8c07e0e32af72083c4
asiful087 <asifulhaque087@gmail.com>
2022-04-11 01:55:19 +0600

    add font awesome

--- src/main.js
+++ src/main.js
@@ -1,6 +1,12 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import "./index.css";
-import SplitCarousel from "vue-split-carousel";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faUser } from "@fortawesome/free-solid-svg-icons";
 
-createApp(App).use(SplitCarousel).mount("#app");
+import { faYoutube } from "@fortawesome/free-brands-svg-icons";
+
+library.add(faUser, faYoutube);
+
+createApp(App).component("fa", FontAwesomeIcon).mount("#app");
--- src/pages/Profile.vue
+++ src/pages/Profile.vue
@@ -121,7 +121,7 @@
               class="flex items-center space-x-2 font-semibold text-gray-900 leading-8"
             >
               <span clas="text-green-500">
-                <svg
+                <!-- <svg
                   class="h-5"
                   xmlns="http://www.w3.org/2000/svg"
                   fill="none"
@@ -134,9 +134,14 @@
                     stroke-width="2"
                     d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z"
                   />
-                </svg>
+                </svg> -->
+                <!-- <fa icon="coffee" /> -->
+                <!-- <fa :icon="['fas', 'coffee']" />
+                <fa :icon="['fab', 'youtube']" /> -->
+
+                <fa icon="user" />
               </span>
-              <span class="tracking-wide">About</span>
+              <span class="tracking-wide">About </span>
             </div>
             <div class="text-gray-700">
               <div class="grid md:grid-cols-2 text-sm">
