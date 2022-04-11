36e486bff66b20dff953f4e7dfe9fb8ff99869f8
asiful087 <asifulhaque087@gmail.com>
2022-04-12 02:04:10 +0600

    modify link

--- src/components/BottomHeader.vue
+++ src/components/BottomHeader.vue
@@ -41,6 +41,15 @@ export default {};
       >
         <li>
           <router-link
+            to="/"
+            class="block py-2 px-4 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+          >
+            Home
+          </router-link>
+        </li>
+
+        <li>
+          <router-link
             to="/shopping-cart"
             class="block py-2 px-4 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
           >
