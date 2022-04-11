b6f5a782106d8a4327528eec423eaf92c3c3382a
asiful086 <asifulhaque086@gmail.com>
2022-04-11 22:59:20 +0600

    add best seller

--- src/components/BestSeller.vue
+++ src/components/BestSeller.vue
@@ -2,20 +2,84 @@
   <div>
     <h1 class="text-2xl font-bold text-[#333] mb-3">Bestseller Products</h1>
     <div class="bg-white border">
-      <div class="flex items-center justify-between">
-        <div>
+      <!-- 1st -->
+      <div class="flex items-center p-3 border-b">
+        <!-- left -->
+        <div class="w-1/2">
           <img
             src="https://velikorodnov.com/wordpress/shopmewp/classic/wp-content/uploads/2015/04/1-350x350.jpg"
             alt=""
           />
         </div>
-        <div>
-          <span> Metus nulla facilisi, Original 24 fl oz </span>
-          <span>
-            <span>$9.99</span>
+        <!-- right -->
+        <div class="">
+          <p class="text-[#333] text-sm leading-normal">
+            Metus nulla facilisi, Original 24 fl oz
+          </p>
+          <span class="flex gap-2 font-bold text-cyan-500 text-lg">
+            <span class="text-gray-300 font-medium line-through">$9.99</span>
             <span>$5.99</span>
           </span>
+          <div class="flex items-center">
+            <fa icon="star" class="text-yellow-400 text-sm" />
+            <fa icon="star" class="text-yellow-400 text-sm" />
+            <fa icon="star-half-stroke" class="text-yellow-400 text-sm" />
+            <fa icon="star-half-stroke" class="text-yellow-400 text-sm" />
+          </div>
+        </div>
+      </div>
 
+      <!-- 2nd -->
+      <div class="flex items-center p-3 border-b">
+        <!-- left -->
+        <div class="w-1/2">
+          <img
+            src="https://velikorodnov.com/wordpress/shopmewp/classic/wp-content/uploads/2015/04/2-350x350.jpg"
+            alt=""
+          />
+        </div>
+        <!-- right -->
+        <div class="">
+          <p class="text-[#333] text-sm leading-normal">
+            Metus nulla facilisi, Original 24 fl oz
+          </p>
+          <span class="flex gap-2 font-bold text-cyan-500 text-lg">
+            <span class="text-gray-300 font-medium line-through">$9.99</span>
+            <span>$5.99</span>
+          </span>
+          <div class="flex items-center">
+            <fa icon="star" class="text-yellow-400 text-sm" />
+            <fa icon="star" class="text-yellow-400 text-sm" />
+            <fa icon="star-half-stroke" class="text-yellow-400 text-sm" />
+            <fa icon="star-half-stroke" class="text-yellow-400 text-sm" />
+          </div>
+        </div>
+      </div>
+
+      <!-- 3rd -->
+      <div class="flex items-center p-3">
+        <!-- left -->
+        <div class="w-1/2">
+          <img
+            src="https://velikorodnov.com/wordpress/shopmewp/classic/wp-content/uploads/2015/04/6-350x350.jpg"
+            alt=""
+          />
+        </div>
+        <!-- right -->
+        <div class="">
+          <p class="text-[#333] text-sm leading-normal">
+            Metus nulla facilisi, Original 24 fl oz
+          </p>
+          <span class="flex gap-2 font-bold text-cyan-500 text-lg">
+            <span class="text-gray-300 font-medium line-through">$9.99</span>
+            <span>$5.99</span>
+          </span>
+          <div class="flex items-center">
+            <fa icon="star" class="text-yellow-400 text-sm" />
+            <fa icon="star" class="text-yellow-400 text-sm" />
+            <fa icon="star-half-stroke" class="text-yellow-400 text-sm" />
+            <fa icon="star-half-stroke" class="text-yellow-400 text-sm" />
+          </div>
         </div>
       </div>
     </div>
